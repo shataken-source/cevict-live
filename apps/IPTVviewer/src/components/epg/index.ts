f6eb2538ea0c23f8e4ecd578101313c69fbeaf57
cevict <shataken@gmail.com>
2026-02-16 19:43:37 -0600

    chore: Remove IPTVviewer deleted files

--- apps/IPTVviewer/src/components/epg/index.ts
+++
@@ -1,4 +0,0 @@
-export { TimeHeader } from './TimeHeader';
-export { ChannelList } from './ChannelList';
-export { ProgramCell } from './ProgramCell';
-export { ProgramDetailModal } from './ProgramDetailModal';
